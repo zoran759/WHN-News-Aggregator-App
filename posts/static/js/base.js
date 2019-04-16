fe08b1eace4817aa9cbfab2112a79c515c0eb987
Zoran Rasic <zokiupwork@gmail.com>
2019-04-16 13:54:48 +0300

    Mobile suppot added

--- posts/static/js/base.js
+++ posts/static/js/base.js
@@ -1,17 +1,11 @@
-$(document).ready(function() {
-	$('.email-share').click(function() {
-		var post_id = $(this).attr("post_id");
-		$('#id_post_id_modal').attr("value",post_id);
-	    });
-    });
 
-$(document).ready(function() {
-	$('.flag-post').click(function() {
-		var post_id = $(this).attr("post_id");
-		$('#flag-post-'+post_id).submit();
-	    });
 
-	$('#register-modal').on('hidden.bs.modal', function () {
-		$('#upvote-register-explanation').hide();
-	    })
-    });
+// $(function () {
+// 	$('.article .btn-upvote').click(function (e) {
+// 		e.preventDefault();
+// 		$.ajax({
+// 			url: "{% url 'vote_post' %}",
+//
+// 		})
+//     });
+// });
