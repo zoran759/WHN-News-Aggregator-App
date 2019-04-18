b657a5137ed2750cb67fe1f33b895e89a5135a57
Zoran Rasic <zokiupwork@gmail.com>
2019-04-19 01:05:23 +0300

    Cookie js added, search design complete and working

--- adventure_agg/urls.py
+++ adventure_agg/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^most_comments/$', old_views.index_most_comments, name='index_most_comments'),
     url(r'^vegan/$', old_views.index_vegan, name='index_vegan'),
     
-    url(r'^search/$', old_views.search, name='search'),
+    url(r'^search/$', views.SearchView.as_view(), name='search'),
     
     url(r'^admin_dashboard/$', old_views.admin_scheduled_posts, name='admin_dashboard'),
     url(r'^admin_dashboard/scheduled_posts/$', old_views.admin_scheduled_posts, name='admin_scheduled_posts'),
--- posts/static/js/base.js
+++ posts/static/js/base.js
@@ -1,40 +1,34 @@
 $(function () {
-	// $('.article .btn-upvote').click(function (e) {
-	// 	e.preventDefault();
-	// 	$.ajax({
-	// 		url: "{% url 'vote_post' %}",
-    //
-	// 	})
-    // });
-
-    let searchInput = $('#search-form input');
-    let suggestNewsButton = $('.suggest-news');
-    searchInput.focus(function () {
-        suggestNewsButton.fadeTo('fast', 0);
-        suggestNewsButton.hide();
+    let search = $('#search-form');
+    let searchButton = $('.btn-search');
+    searchButton.click(function () {
+        search.animate({height: 'toggle'}, 250, function () {
+            search.children().focus();
+        });
+        if (search.css('display') !== 'none') {
+            $('.main-content').css('display', 'block');
+            $('#search-results').css('display', 'none');
+        }
     });
 
-    searchInput.focusout(function () {
-        setTimeout(function () {
-            suggestNewsButton.show();
-            suggestNewsButton.fadeTo('fast', 1);
-        }, 400)
-    });
+    function searchSend(str) {
+        $.ajax({
+            method: 'GET',
+            url: '/search/',
+            data: {'q': str},
+            success: function (html) {
+                $('.main-content').css('display', 'none');
+                $('#search-results').html(html).css('display', 'block');
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    }
 
-
-    $('.article .article-title').on('click', function () {
-        let url = $(this).closest('.article').data('url');
-        // $.ajax({
-        //     type: 'GET',
-        //     url: url,
-        //     success: function (html) {
-        //         let modal = $('#PostModal');
-        //         modal.find('.modal-content').html(html);
-        //         modal.modal('show');
-        //     },
-        //     error: function (data, textStatus) {
-        //         console.log(data, textStatus);
-        //     }
-        // });
+    var timeoutID = null;
+    $('#search-form input').on('keyup click', function (e) {
+        clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => searchSend(e.target.value), 250);
     });
 });
