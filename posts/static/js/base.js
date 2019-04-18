18fc1fe932350f1bbff4a1b3c9e3cb1ccecf7087
Zoran Rasic <zokiupwork@gmail.com>
2019-04-18 13:44:17 +0300

    Search input ready

--- posts/static/js/base.js
+++ posts/static/js/base.js
@@ -1,11 +1,37 @@
+$(function () {
+	// $('.article .btn-upvote').click(function (e) {
+	// 	e.preventDefault();
+	// 	$.ajax({
+	// 		url: "{% url 'vote_post' %}",
+    //
+	// 	})
+    // });
+
+    let searchInput = $('#search-form input');
+    searchInput.focus(function () {
+       $('.suggest-news').hide();
+    });
+
+    searchInput.focusout(function () {
+        setTimeout(function () {
+            $('.suggest-news').show();
+        }, 400)
+    });
 
 
-// $(function () {
-// 	$('.article .btn-upvote').click(function (e) {
-// 		e.preventDefault();
-// 		$.ajax({
-// 			url: "{% url 'vote_post' %}",
-//
-// 		})
-//     });
-// });
+    $('.article .article-title').on('click', function () {
+        let url = $(this).closest('.article').data('url');
+        // $.ajax({
+        //     type: 'GET',
+        //     url: url,
+        //     success: function (html) {
+        //         let modal = $('#PostModal');
+        //         modal.find('.modal-content').html(html);
+        //         modal.modal('show');
+        //     },
+        //     error: function (data, textStatus) {
+        //         console.log(data, textStatus);
+        //     }
+        // });
+    });
+});
