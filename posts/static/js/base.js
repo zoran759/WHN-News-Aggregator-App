0c85c5eafaab080a4848f0a5718cac988e426e6a
Zoran Rasic <zokiupwork@gmail.com>
2019-04-19 16:19:12 +0300

    pagination on search

--- posts/static/js/base.js
+++ posts/static/js/base.js
@@ -1,9 +1,10 @@
 $(function () {
     let search = $('#search-form');
     let searchButton = $('.btn-search');
+    let searchInput = $('#search-form input');
     searchButton.click(function () {
         search.animate({height: 'toggle'}, 250, function () {
-            search.children().focus();
+            searchInput.focus();
         });
         if (search.css('display') !== 'none') {
             $('.main-content').css('display', 'block');
@@ -11,11 +12,11 @@ $(function () {
         }
     });
 
-    function searchSend(str) {
+    function searchSend(str, pageNumber=1) {
         $.ajax({
             method: 'GET',
             url: '/search/',
-            data: {'q': str},
+            data: {'q': str, 'page': pageNumber},
             success: function (html) {
                 $('.main-content').css('display', 'none');
                 $('#search-results').html(html).css('display', 'block');
@@ -27,8 +28,19 @@ $(function () {
     }
 
     var timeoutID = null;
-    $('#search-form input').on('keyup click', function (e) {
+    let pageNumber = 1;
+    $('#search-results').on('click', '.btn-pagination', function (e) {
+        let next_page = $(this).data('page');
+        searchSend(searchInput.val(), next_page);
+    });
+
+    search.on('submit', function (e) {
+        e.preventDefault();
+        searchSend(event.target.querySelector('input').value, pageNumber);
+    });
+
+    searchInput.on('keyup', function (e) {
         clearTimeout(timeoutID);
-      timeoutID = setTimeout(() => searchSend(e.target.value), 250);
+      timeoutID = setTimeout(() => searchSend(e.target.value, pageNumber), 250);
     });
 });
