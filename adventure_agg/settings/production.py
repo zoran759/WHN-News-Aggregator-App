1c4b6d22c612fc40d4a62e5d1c17c8295783c5b4
Zoran Rasic <zokiupwork@gmail.com>
2019-04-27 00:58:24 +0300

    Updating django settings are set and requirements

---
+++ adventure_agg/settings/production.py
@@ -0,0 +1,15 @@
+from .base import *
+
+ALLOWED_HOSTS = ["news.viceroy.tech"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        "NAME": 'news_aggregator',  # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        "USER": "news_aggregator_user",
+        "PASSWORD": "&?6<&MUXr3#r^,",
+        "HOST": "localhost",  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        "PORT": "5432",  # Set to empty string for default.
+    }
+}
