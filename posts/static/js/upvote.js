e3cfc72c82ce2c7d5925c19853463e4ece888cef
Zoran Rasic <zokiupwork@gmail.com>
2019-07-14 20:53:57 +0300

    Upvote change for speed

--- posts/static/js/upvote.js
+++ posts/static/js/upvote.js
@@ -3,26 +3,38 @@ $(function () {
     $(document).on('click touch', '.btn-upvote', function (e) {
         var rating_number = $(this).find('.rating-text');
         var button = $(this);
+        if (button.hasClass('modal-article-vote')) {
+            let article = button.parents('.article').data('article');
+            button = $('.article[data-article="' + article + '"]').find('.btn-upvote');
+            rating_number = button.find('.rating-text');
+        }
+        if (!button.hasClass('active')) {
+            rating_number.html(Math.sign(rating_number.html()) + 1);
+            button.addClass('active');
+        } else {
+            rating_number.html(Math.sign(rating_number.html()) - 1);
+            button.removeClass('active');
+        }
         $.ajax({
             method: 'POST',
             url: button.data('vote'),
             data: {
                 'csrfmiddlewaretoken': Cookies.get('csrftoken'),
             },
-            success: function (vote) {
-                if (button.hasClass('modal-article-vote')) {
-                    let article = button.parents('.article').data('article');
-                    button = $('.article[data-article="' + article + '"]').find('.btn-upvote');
-                    rating_number = button.find('.rating-text');
-                }
-                if (vote['up_vote']) {
-                    rating_number.html(vote['score']);
-                    button.addClass('active');
-                } else {
-                    rating_number.html(vote['score']);
-                    button.removeClass('active');
-                }
-            },
+            // success: function (vote) {
+            //     if (button.hasClass('modal-article-vote')) {
+            //         let article = button.parents('.article').data('article');
+            //         button = $('.article[data-article="' + article + '"]').find('.btn-upvote');
+            //         rating_number = button.find('.rating-text');
+            //     }
+            //     if (vote['up_vote']) {
+            //         rating_number.html(vote['score']);
+            //         button.addClass('active');
+            //     } else {
+            //         rating_number.html(vote['score']);
+            //         button.removeClass('active');
+            //     }
+            // },
             error: function (err) {
                 console.log(err);
             }
